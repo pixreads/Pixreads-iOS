3927fa284e2767503f298070befa64776478f336
Ahmed Sallam <ahmedsallamdd@gmail.com>
2026-05-20 12:09:50 +0300

    init

--- Package.swift
+++ Package.swift
@@ -15,6 +15,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "Pixreads",
+            url: "https://github.com/pixreads/Pixreads-iOS/releases/download/1.0.0/Pixreads.xcframework.zip",
+            checksum: "0f3b4cf1672dd9a6cb1f7b4c929b04021a16c187747a0406d9187e254742f26c"
+        ),
         .target(
             name: "Pixreads"
         ),
