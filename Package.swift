ea460785401e34e273901111ebaa7bff0134d335
Ahmed Sallam <ahmedsallamdd@gmail.com>
2026-05-20 12:42:48 +0300

    added platform explicitly

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Pixreads",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,10 +20,7 @@ let package = Package(
             name: "Pixreads",
             url: "https://github.com/pixreads/Pixreads-iOS/releases/download/1.0.0/Pixreads.xcframework.zip",
             checksum: "0f3b4cf1672dd9a6cb1f7b4c929b04021a16c187747a0406d9187e254742f26c"
-        ),
-        .target(
-            name: "Pixreads"
-        ),
+        )
 
     ]
 )
